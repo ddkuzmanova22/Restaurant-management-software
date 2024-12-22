741b5f46ce05166e62adb0e7eebc6f9775834073
ddkuzmanova22 <daniela_kuzmanova2205@abv.bg>
2024-12-23 00:05:46 +0200

    Add files via upload

---
+++ Restaurant-management-software.cpp
@@ -0,0 +1,25 @@
+// Restaurant-management-software.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+/**
+* 
+* Solution to course project #1
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2024/2025
+* 
+* @author Daniela Kuzmanova
+* @idnumber 7MI0600539
+* @compiler VS
+* 
+* file project
+* 
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
